ceeb3ffc98279a529880d647a8e08d8c94822b0a
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-10-14 19:10:13 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# NodeSharedClipboard
+# Node Shared Clipboard
+
+## Credits
+
+- Inspired by [Quackduck's Uniclip](https://github.com/quackduck/uniclip)
+- Original idea by [Aaryan Porwal](https://github.com/aaryanporwal)
+
+## Running
+
+Run
+
+```
+node app.js
+```
---
+++ app.js
@@ -0,0 +1,63 @@
+const monitor = require('clipboard-monitor')()
+const clipboardy = require('clipboardy')
+
+const listenServer = _ => {
+    console.log("RUNNING AS SERVER\nWARNING: THE PANDAS ARE COMING")
+    const app = require("express")()
+    const server = require("http").createServer(app)
+    const io = require("socket.io")(server)
+    
+    server.listen(process.env.PORT || 0, _ => {
+        console.log(`Listening on port ${process.env.PORT || server.address().port}`)
+    })
+
+    io.on('connection', socket => {
+        monitor.on('copy', data => {
+            console.log(`Copied: ${data}`)
+            socket.emit('copy', data)
+        })
+        console.log(`Connected to ${socket.id}`)    
+        socket.on('copy', async data => {
+            console.log(`Got data: ${data}`)
+            await clipboardy.write(data)
+        })
+    })
+}
+
+const connectClient = _ => {
+    console.log("RUNNING AS CLIENT\nWARNING: THE PANDAS ARE COMING")
+    let socket
+    try {
+        socket = require('socket.io-client')(process.argv[2])
+    } catch {
+        throw "Invalid server address"
+    }
+
+    console.log(process.argv[2])
+
+    socket.on('connect', _ => {
+        console.log(`Connected to ${process.argv[2]}`)
+        monitor.on('copy', data => {
+            console.log(`Copied: ${data}`)
+            socket.emit('copy', data)
+        })
+    })
+    socket.on('copy', async data => {
+        console.log(`Got data: ${data}`) 
+        await clipboardy.write(data)
+    })
+}
+
+try {
+    if (process.argv.length === 2) { // Server
+        listenServer()
+    } else if (process.argv.length === 3) { // Client
+        connectClient()
+    } else {
+        throw "Invalid arguments"
+    }
+} catch (e) {
+    console.log(e)
+    console.log(`Usage: ${process.argv[0]} ${process.argv[1]} [server-address if running as client]`)
+    process.exit(1)
+}
